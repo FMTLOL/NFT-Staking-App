5a93b09185101c02ce423b54354544cef9e1fca6
FMS <71218677+FMTLOL@users.noreply.github.com>
2024-01-23 06:16:03 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Navbar } from "../components/Navbar/Navbar";
+import ThirdwebGuideFooter from "../components/ThirdwebGuideFooter";
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
@@ -8,18 +10,16 @@ const activeChain = "polygon";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider
-      activeChain={activeChain}
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-    >
-      <Component {...pageProps} />
-      
+    <ThirdwebProvider activeChain={activeChain}>
+      <Head>
+        <title>FMS Staking</title>
+      </Head>
       <Navbar />
+      
       <Component {...pageProps} />
+      <ThirdwebGuideFooter />
     </ThirdwebProvider>
-    
   );
-  
 }
 
 export default MyApp;
