a23134034902625bde2a6ea6499ecd20c69f5ef2
FMS <71218677+FMTLOL@users.noreply.github.com>
2024-01-24 16:34:38 +0200

    Update mint.tsx

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -10,15 +10,16 @@ const Mint: NextPage = () => {
 
   return (
     <div className={styles.container}>
+      <Image src="/nft.png" alt="logo" width={300} height={200} /> 
+      <br />
+      <br />
       <h1 className={styles.h1}>FMS Discount NFT</h1>
       <br />
       <br />
-      <Image src="/nft.png" alt="logo" width={300} height={200} /> 
       <h4>3% discount on all FMS platform services</h4>
       <p className={styles.explain}>
       MINT Price 200.000 FMS Tokens 
       </p>
-      
      <br />
       <Web3Button
         connectWallet={{
