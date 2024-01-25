e6dee11dc92ade2661f176638a08947e94cbf8e9
FMS <71218677+FMTLOL@users.noreply.github.com>
2024-01-25 10:10:29 +0200

    Update contractAddresses.ts

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -1,6 +1,6 @@
 export const nftDropContractAddress =
   "0xC1f09A0792683677F5A16ceEB72B637cA8Ab12C9";
 export const tokenContractAddress =
-  "0x649EBee9cddf8373376dc7AEAbE8B975fC8706b8";
+  "0x4D3114B43fB220CE0E60C2F94495Ab8Ce9B2FA9d";
 export const stakingContractAddress =
-  "0xE681C56ff9b534dd8c7Df4F405CBcc2081AA2299";
+  "0xe057C5BE65014aC04bC7eCD202A410e55Df6a1bB";
